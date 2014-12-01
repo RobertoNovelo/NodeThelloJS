ae3226d333a365e53da262d5ffdf75d9b520c8b8
Roberto Novelo <robertod.novelo@gmail.com>
2014-12-01 05:24:12 -0600

    Game Initialization
    Game between 2 AIs set to test the game flow.

--- public/javascripts/othello-game.js
+++ public/javascripts/othello-game.js
@@ -1,15 +1,71 @@
+var othelloBoard = null;
+var othelloAI = null;
+var player1 = null;
+var player2 = null;
+/*const*/ var AI = "AI";
+/*const*/ var USER = "USER";
+/*const*/ var BLACK = "black";
+/*const*/ var WHITE = "white";
+/*const*/ var EMPTY = "empty";
+/*const*/ var USERMOVES = "user-moves";
+
 
 $(function()
 {
+
+	$(".user-moves").on('click', function()
+	{
+	});
 
 });
 
 $(document).ready(function() 
 {
-	
+	othelloBoard = new OthelloBoard();
+	othelloBoard.setBoard(othelloBoard.getBoardFromHtml('board'));
+	startGame();
 	
 });
 
-$.ajaxSetup({
-    cache: false
-});
+function startGame()
+{
+	player1 = new Player(AI,BLACK);
+	player2 = new Player(AI,WHITE);
+
+	othelloAI = new OthelloAI();
+
+	move(player1,player2);
+}
+
+function move(playerMoving,playerWaiting)
+{
+	if(AI == playerMoving.intel)
+	{
+		othelloAI.setNextMove(othelloBoard,playerMoving.color);
+		
+		if(AI == playerWaiting.intel)
+		{
+			setTimeout(function()
+			{
+				move(playerWaiting,playerMoving);
+			},1000);
+		}
+		else
+		{
+			othelloAI.setUserPossibleMoves(othelloBoard,playerWaiting.color);
+		}
+	}
+}
+
+function Player(userIntel, userColor)
+{
+	this.intel = userIntel;
+	this.color = userColor;
+
+}
+
+
+
+
+
+
